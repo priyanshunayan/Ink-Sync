75bd750e4d4ecb38ec5d5563fd6c5bf8b9455cb8
Priyanshu Nayan <priyanshunayan456@gmail.com>
2024-03-31 17:34:29 +0530

    Initial Commit

---
+++ WriteGPT/WriteGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WriteGPTApp.swift
+//  WriteGPT
+//
+//  Created by Priyanshu Nayan on 31/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WriteGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
