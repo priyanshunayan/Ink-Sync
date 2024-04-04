b7a662008d400b1426ba560632a5e8e91f162c0c
Priyanshu Nayan <priyanshunayan456@gmail.com>
2024-04-04 22:37:29 +0530

    Fix window comming up on launch of app and also read open ai key from the settings screen

---
+++ WriteGPT/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  WriteGPT
+//
+//  Created by Priyanshu Nayan on 03/04/24.
+//
+
+import Foundation
---
+++ WriteGPT/SettingsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsScreen.swift
+//  WriteGPT
+//
+//  Created by Priyanshu Nayan on 03/04/24.
+//
+
+import SwiftUI
+
+struct SettingsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsScreen()
+}
